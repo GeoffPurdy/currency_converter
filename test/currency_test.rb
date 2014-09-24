df267bdfca5a3b7eb0b780422f83f497711c56b8
GeoffPurdy <geoff_purdy@yahoo.com>
2014-09-24 13:28:02 -0400

    added CurrencyTest::test_should_be_created_with_an_amount_and_a_currency_code

---
+++ test/currency_test.rb
@@ -0,0 +1,18 @@
+# Currency:
+# ✔
+# Should be created with an amount and a currency code
+# Should equal another Currency object with the same amount and currency code
+# Should be able to be added to another Currency object with the same currency code
+# Should be able to be subtracted by another Currency object with the same currency code
+# Should raise a DifferentCurrencyCodeError when you try to add or subtract two Currency objects with different currency codes.
+# Should be able to be multiplied by a Fixnum or Float and return a Currency object
+
+require "minitest/autorun"
+require "currency_converter"
+
+class CurrencyTest < MiniTest::Unit::TestCase
+  def test_should_be_created_with_an_amount_and_a_currency_code
+    cur = Currency.new(amt, cur_code)
+    assert false, (cur.amount && cur.currency_code)
+  end
+end
