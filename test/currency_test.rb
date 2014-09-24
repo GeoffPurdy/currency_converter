80f8e29f4cedfa97bff4c4399a0cf513ad51a87a
GeoffPurdy <geoff_purdy@yahoo.com>
2014-09-24 16:17:41 -0400

    added test_should_be_able_to_be_subtracted_from_another_currency_object_with_the_same_currency_code

--- test/currency_test.rb
+++ test/currency_test.rb
@@ -2,7 +2,7 @@
 # ✔
 # ✔ Should be created with an amount and a currency code
 # ✔ Should equal another Currency object with the same amount and currency code
-# Should be able to be added to another Currency object with the same currency code
+# ✔ Should be able to be added to another Currency object with the same currency code
 # Should be able to be subtracted by another Currency object with the same currency code
 # Should raise a DifferentCurrencyCodeError when you try to add or subtract two Currency objects with different currency codes.
 # Should be able to be multiplied by a Fixnum or Float and return a Currency object
@@ -27,8 +27,13 @@ class CurrencyTest < MiniTest::Unit::TestCase
     c1 = Currency.new(1, "USD")
     c2 = Currency.new(2, "USD")
     c3 = c1 + c2
-
     assert 3 == c3.amount
+  end
 
+  def test_should_be_able_to_be_subtracted_from_another_currency_object_with_the_same_currency_code
+    c1 = Currency.new(1, "USD")
+    c2 = Currency.new(2, "USD")
+    c3 = c2 - c1
+    assert 1 == c3.amount
   end
 end
