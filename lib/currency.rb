ff06bc06eb87e0400a51ab797b1c415fab029cbc
GeoffPurdy <geoff_purdy@yahoo.com>
2014-09-24 13:27:45 -0400

    added CurrencyTest::test_should_be_created_with_an_amount_and_a_currency_code

---
+++ lib/currency.rb
@@ -0,0 +1,15 @@
+# Currency:
+# ✔
+# Should be created with an amount and a currency code
+# Should equal another Currency object with the same amount and currency code
+# Should be able to be added to another Currency object with the same currency code
+# Should be able to be subtracted by another Currency object with the same currency code
+# Should raise a DifferentCurrencyCodeError when you try to add or subtract two Currency objects with different currency codes.
+# Should be able to be multiplied by a Fixnum or Float and return a Currency object
+
+
+class Currency
+  def run
+    fail "You need to add some functionality here before you can use this."
+  end
+end
