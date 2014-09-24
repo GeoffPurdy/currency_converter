697221b0b9d1acd2d2e6f46799078ca98865457d
GeoffPurdy <geoff_purdy@yahoo.com>
2014-09-24 14:54:54 -0400

    implemented     c1 = Currency.new(1, USD)

--- test/currency_test.rb
+++ test/currency_test.rb
@@ -1,7 +1,7 @@
 # Currency:
 # ✔
 # ✔ Should be created with an amount and a currency code
-# Should equal another Currency object with the same amount and currency code
+# ✔ Should equal another Currency object with the same amount and currency code
 # Should be able to be added to another Currency object with the same currency code
 # Should be able to be subtracted by another Currency object with the same currency code
 # Should raise a DifferentCurrencyCodeError when you try to add or subtract two Currency objects with different currency codes.
@@ -14,12 +14,21 @@ require "currency"
 class CurrencyTest < MiniTest::Unit::TestCase
   def test_should_be_created_with_an_amount_and_a_currency_code
     cur = Currency.new(2, "USD")
-    assert true, (cur.amount && cur.currency_code)
+    assert (cur.amount && cur.currency_code)
   end
 
-  def should_equal_another_currency_object_with_the_same_amount_and_currency_code
+  def test_should_equal_another_currency_object_with_the_same_amount_and_currency_code
     c1 = Currency.new(2, "USD")
     c2 = Currency.new(2, "USD")
-    assert false, c1 == c2
+    assert c1 == c2
+  end
+
+  def test_should_be_able_to_be_added_to_another_currency_object_with_the_same_currency_code
+    c1 = Currency.new(1, "USD")
+    c2 = Currency.new(2, "USD")
+    c3 = c1 + c2
+
+    assert 3 == c3.amount
+
   end
 end
