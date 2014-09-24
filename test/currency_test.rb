222e88296e09544a8c14c7db25cdcf880fe62ef8
GeoffPurdy <geoff_purdy@yahoo.com>
2014-09-24 13:52:56 -0400

    added Currency::should_equal_another_currency_object_with_the_same_amount_and_currency_code

--- test/currency_test.rb
+++ test/currency_test.rb
@@ -1,6 +1,6 @@
 # Currency:
 # ✔
-# Should be created with an amount and a currency code
+# ✔ Should be created with an amount and a currency code
 # Should equal another Currency object with the same amount and currency code
 # Should be able to be added to another Currency object with the same currency code
 # Should be able to be subtracted by another Currency object with the same currency code
@@ -15,5 +15,11 @@ class CurrencyTest < MiniTest::Unit::TestCase
   def test_should_be_created_with_an_amount_and_a_currency_code
     cur = Currency.new(2, "USD")
     assert true, (cur.amount && cur.currency_code)
+  end
+
+  def should_equal_another_currency_object_with_the_same_amount_and_currency_code
+    c1 = Currency.new(2, "USD")
+    c2 = Currency.new(2, "USD")
+    assert true, c1 == c2
   end
 end
