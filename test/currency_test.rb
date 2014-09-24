efcc86f001d4fdbb248fe470d5f3e0a29413f60d
GeoffPurdy <geoff_purdy@yahoo.com>
2014-09-24 16:57:06 -0400

    test_should_be_created_with_a_Hash_of_currency_codes_to_conversion_rates

--- test/currency_test.rb
+++ test/currency_test.rb
@@ -1,11 +1,11 @@
 # Currency:
-# ✔
+# 
 # ✔ Should be created with an amount and a currency code
 # ✔ Should equal another Currency object with the same amount and currency code
 # ✔ Should be able to be added to another Currency object with the same currency code
 # ✔ Should be able to be subtracted by another Currency object with the same currency code
 # ✔ Should raise a DifferentCurrencyCodeError when you try to add or subtract two Currency objects with different currency codes.
-# Should be able to be multiplied by a Fixnum or Float and return a Currency object
+# ✔ Should be able to be multiplied by a Fixnum or Float and return a Currency object
 
 require "minitest/autorun"
 require "currency_converter"
